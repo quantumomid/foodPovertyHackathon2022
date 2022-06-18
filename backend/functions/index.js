11d03b3f7bb3f48698da1062b44ec10c1171b8dc
Ameera Al-shaibani <ameera.al@hotmail.co.uk>
2022-06-18 15:38:51 +0100

    Add express

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,49 +1,46 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
+app.get('/recipient', async(req, res) => {
     // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    const recipientCode = req.query.text;
+
+    const result = await admin.firestore().collection('recipient')
+        .where('id','==', recipientCode)
+        .get();
+
     // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+    res.json( result.docs[0].data());
+    res.end("Received GET request!");
 });
 
-// Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-    .onCreate((snap, context) => {
-        // Grab the current value of what was written to Firestore.
-        const original = snap.data().original;
+app.post('/recipient', async(req, res) => {
+    // Grab the text parameter.
+    const original = req.body;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('recipient').add(original);
+    // Send back a message that we've successfully written the message
+    res.json({result: `Recipient with ID: ${writeResult.id} created.`});
+    res.end("Received POST request!");
+});
 
-        // Access the parameter `{documentId}` with `context.params`
-        functions.logger.log('Uppercasing', context.params.documentId, original);
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(app);
 
-        const uppercase = original.toUpperCase();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to Firestore.
-        // Setting an 'uppercase' field in Firestore document returns a Promise.
-        return snap.ref.set({uppercase}, {merge: true});
-    });
-
-
-exports.createRecipient = functions.https.onRequest(async (req, res) => {
+/* exports.createRecipient = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const original = req.body;
     // Push the new message into Firestore using the Firebase Admin SDK.
@@ -57,8 +54,10 @@ exports.getRecipient = functions.https.onRequest(async (req, res) => {
     // Grab the text parameter.
     const recipientCode = req.query.text;
 
-    const result = await admin.firestore().collection('recipient').where('id','==', recipientCode).get();
+    const result = await admin.firestore().collection('recipient')
+        .where('id','==', recipientCode)
+        .get();
 
     // Send back a message that we've successfully written the message
-    res.json({result: result});
-});
+    res.json( result.docs[0].data());
+}); */
