cb61ccafc02f3eecbc1ecf56294fee767de0f4b8
Ameera Al-shaibani <ameera.al@hotmail.co.uk>
2022-06-18 17:35:16 +0100

    Add barcode and category queries

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -6,32 +6,80 @@ const app = express();
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
+const {error} = require("firebase-functions/logger");
 admin.initializeApp();
+
+const db = admin.firestore();
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
-app.get('/recipient', async(req, res) => {
+app.get('/recipient/:recipientCode', async(req, res) => {
     // Grab the text parameter.
-    const recipientCode = req.query.text;
+    const recipientCode = req.params.recipientCode;
 
-    const result = await admin.firestore().collection('recipient')
-        .where('id','==', recipientCode)
+    const result = await db.collection('recipient')
+        .where('united_nations_id','==', recipientCode)
         .get();
 
     // Send back a message that we've successfully written the message
-    res.json( result.docs[0].data());
-    res.end("Received GET request!");
+    res.json(result.docs[0].data());
+});
+
+app.get('/recipient', async(req, res) => {
+    // Grab the barcode parameter.
+    const barcode = req.query.barcode;
+    const category = req.query.category;
+
+    let querySnapshot;
+
+    if (barcode) {
+         querySnapshot = await db.collection('recipient')
+            .where('barcode', '==', barcode)
+            .get();
+    } else if (category) {
+
+        const Category = {
+            FAMILY: "FAMILY",
+            INDIVIDUAL: "INDIVIDUAL"
+        };
+
+        if (category in Category){
+            querySnapshot = await db.collection('recipient')
+                .where('category', '==', category)
+                .get();
+        } else {
+            res.status(400).end("Invalid Category!");
+            return;
+        }
+    } else {
+         querySnapshot = await db.collection('recipient').get();
+    }
+
+    // Send back a message that we've successfully written the message
+    const result = querySnapshot.docs.map(doc => doc.data());
+    res.json(result);
 });
 
 app.post('/recipient', async(req, res) => {
     // Grab the text parameter.
     const original = req.body;
     // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('recipient').add(original);
-    // Send back a message that we've successfully written the message
-    res.json({result: `Recipient with ID: ${writeResult.id} created.`});
-    res.end("Received POST request!");
+    const recipientCode = req.body.united_nations_id;
+
+    const result = await db.collection('recipient')
+        .where('united_nations_id','==', recipientCode)
+        .get();
+
+    if (result.docs.length > 0){
+        res.status(400)
+            .json({result: `Recipient with ID: ${recipientCode} already exists`});
+    } else {
+        const writeResult = await db.collection('recipient').doc(recipientCode).set(original);
+        // Send back a message that we've successfully written the message
+        //TODO get id from write result
+        res.json({result: `Recipient with ID: ${writeResult} created.`});
+    }
 });
 
 // Expose Express API as a single Cloud Function:
