17a0afd042d8ac7433cd8d3190635387f7b65502
quantumomid <omid.wakili@outlook.com>
2022-06-18 17:30:23 +0100

    login functionality with firebase for on submitting login form

--- src/components/login/LogInForm.js
+++ src/components/login/LogInForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Flex, Button, ButtonGroup } from "@chakra-ui/react";
+import { signInWithEmailAndPassword } from "@firebase/auth";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 
@@ -7,8 +8,21 @@ export default function LogInForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            setEmail("");
+            setPassword("");
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
     return (
-        <Flex as="form" flexDir="column" px="5" h="70vh">
+        <Flex as="form" flexDir="column" px="5" h="70vh" onSubmit={handleLoginSubmit}>
             <EmailInput email={email} setEmail={setEmail} />
             <PasswordInput password={password} setPassword={setPassword} />
             <ButtonGroup 
@@ -16,7 +30,8 @@ export default function LogInForm() {
                 gap="4"
             >
                 <Button 
-                    colorScheme="teal" 
+                    colorScheme="teal"
+                    type="submit" 
                 >
                     Log in
                 </Button>
