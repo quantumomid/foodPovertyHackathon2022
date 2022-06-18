94c18869decb2fa3434aa34dcbeb055b42e8d193
quantumomid <omid.wakili@outlook.com>
2022-06-18 12:07:44 +0100

    set up firebase integration and redux for managing user in next.js

---
+++ frontend/next-app/components/layout/Layout.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { onSnapshot } from "firebase/firestore";
+import { auth, createUserProfileDocument } from "../../firebase/firebaseUtils";
+import { setCurrentUser } from "../../redux/user/userActions";
+
+const Layout = ({ children }) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      const unsubscribeFromAuth = onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          console.log("user is logged in");
+          const userRef = await createUserProfileDocument(user);
+          onSnapshot(userRef, (snapShot) => {
+            dispatch(
+              setCurrentUser({
+                id: snapShot.id,
+                ...snapShot.data(),
+              })
+            );
+          });
+        } else {
+          // User is signed out
+          console.log("Layout.js, no user is logged in!");
+          // i.e. setting our redux current user state to null
+          dispatch(setCurrentUser(user));
+        }
+      });
+  
+      // Below is to clean up when component unmounts, i.e. prevent memory leaks by
+      // unsubscribing from auth.
+      return () => {
+        unsubscribeFromAuth();
+      };
+    }, [dispatch]);
+
+    return (
+      <React.Fragment>
+        { children }
+      </React.Fragment>
+    )
+}
+
+export default Layout;
--- frontend/next-app/pages/_app.js
+++ frontend/next-app/pages/_app.js
@@ -1,10 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import Layout from "../components/layout/Layout";
 // import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
     </ChakraProvider>
   )
 }
