0eead85c5fd4257311331eaa8d33648601e809a6
khadija nur <khadija.nur99@gmail.com>
2022-06-19 10:11:39 +0100

    Formats using prettier and restructures folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,30 +4,15 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <Flex 
-      flexDir="column" justifyContent="center" alignItems="center"
-      py="20" px="8"
-      >
+    <Flex flexDir="column" justify="center" alignItems="center" py="20" px="8">
       <Head>
         <title>FP Hackathon | ⚡</title>
         <meta name="description" content="A tool to help food bank manage inventory and reciepient details." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex
-        flexDir="column"
-        justifyContent="center"
-        pb="32"
-      >
-        <Heading pb="8">
-          Company name here
-        </Heading>
-        <Image 
-          src="/images/logo.png"
-          alt="Logo of a green circle"
-          width={200}
-          height={200}
-          objectFit="contain"
-        />
+      <Flex flexDir="column" justify="center" pb="32">
+        <Heading pb="8">Company name here</Heading>
+        <Image src="/images/logo.png" alt="Logo of a green circle" width={200} height={200} objectFit="contain" />
       </Flex>
       <Text fontSize="24px" fontWeight="500" textAlign="center" pb="24">
         Get started to begin delivering packages to refugess and help make a difference!
@@ -36,5 +21,5 @@ export default function HomePage() {
         Get Started
       </Button>
     </Flex>
-  )
+  );
 }
