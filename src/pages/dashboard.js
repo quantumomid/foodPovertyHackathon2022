6377e91d0aac074c73485fef9d313c4aa0eb0113
khadija nur <khadija.nur99@gmail.com>
2022-06-20 15:18:59 +0100

    Adds transition to landing page and dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,12 @@
-import { VStack, Box, Heading } from "@chakra-ui/react";
-import Navigation from "../components/Card/Navigation";
-import { BsBoxSeam, BsGear, BsSearch, BsPlusCircle, BsTruck } from "react-icons/bs";
-import Banner from "../components/dashboard-banner/Banner";
+import { Fade, VStack, Heading } from "@chakra-ui/react";
+import { BsBoxSeam, BsGear, BsPlusCircle, BsSearch, BsTruck } from "react-icons/bs";
 import withAuthenticatedHOC from "../components/authentication/withAuthenticationHOC";
+import Navigation from "../components/Card/Navigation";
+import Banner from "../components/dashboard-banner/Banner";
+import { useSelector } from "react-redux";
 
 function DashboardPage() {
+  const user = useSelector(state => state.user.currentUser);
   const navLinks = [
     { text: "Deliver a package", icon: <BsBoxSeam />, path: "/deliver" },
     { text: "Search for a refugee", icon: <BsSearch />, path: "/search" },
@@ -12,15 +14,19 @@ function DashboardPage() {
     { text: "View all deliveries", icon: <BsTruck />, path: "/packages" },
     { text: "My settings", icon: <BsGear />, path: "/settings" }
   ];
+
   return (
     <>
       <Banner heading="My Dashboard" />
+      <Fade in={"true"}  style={{ transitionDuration: "300ms" }}>
       <VStack spacing={4} align="stretch" m="5" mt="20">
-        {/* <Heading>username here</Heading> */}
+        <Heading size="md" colour={"grey"} >{user.firstName} {user.lastName}</Heading>
         {navLinks.map((link, i) => (
           <Navigation key={i} text={link.text} color="black" path={link.path} icon={link.icon} />
         ))}
+       
       </VStack>
+      </Fade>
     </>
   );
 }
